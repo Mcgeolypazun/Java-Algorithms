77f0777f0f2740a26956dc38060b73aeecbd4803
Mcgeolypazun <taratula0504@gmail.com>
2023-11-12 19:38:01 +0900

    feat 바둑이 승차문제 풀기

--- src/november12th/Main.java
+++ src/november12th/Main.java
@@ -1,21 +1,53 @@
 package november12th;
 
+
 import java.util.Scanner;
 
 public class Main {
-    class Solution {
-        public int solution(int num1, int num2) {
-            int answer = num1*num2;
-            return answer;
+    static int ch[];
+    static int dogsWeight[];
+    static int count = 0;
+    static int truck;
+    void DFS(int v,int currentCount){
+
+
+        if(count < currentCount && truck > currentCount){
+            count = currentCount;
         }
+        if(v == dogsWeight.length) return;
+        DFS(v+1,currentCount+dogsWeight[v]);
+        DFS(v+1,currentCount);
     }
+
 
     public static void main(String[] args) {
         Main T = new Main();
-        Main.Solution TS = T.new Solution();
         Scanner kb = new Scanner(System.in);
-        int num1 = kb.nextInt();
-        int num2 = kb.nextInt();
-        System.out.println(TS.solution(num1, num2));
+        truck = kb.nextInt();
+        int dogs = kb.nextInt();
+        dogsWeight = new int[dogs];
+        for(int i=0;i<dogs;i++){
+            dogsWeight[i] = kb.nextInt();
+        }
+        int v = 0;
+        int currentCount = 0;
+
+        T.DFS(v,currentCount);
+        System.out.println(count);
+
+    }
+}
+
+
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+
+        for(int i=0;i<numbers.length;i++){
+            answer += numbers[i];
+        }
+        answer = (double)answer/numbers.length;
+
+        return answer;
     }
 }
