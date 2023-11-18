c4da8a9be4eb05be7f1b99c2d4c3b2ee60e2510a
Mcgeolypazun <taratula0504@gmail.com>
2023-11-18 17:00:42 +0900

    feat : enum 열거형 연습

---
+++ src/november18th/Solution.java
@@ -0,0 +1,4 @@
+package november18th;
+
+public class Solution {
+}
---
+++ src/november18th/TrafficLight.java
@@ -0,0 +1,34 @@
+package november18th;
+
+public enum TrafficLight {
+    RED("Stop") {
+        @Override
+        public void mean() {
+            System.out.println("멈추세요!");
+        }
+    },
+    GREEN("Go") {
+        @Override
+        public void mean() {
+            System.out.println("가세요!");
+        }
+    },
+
+    Yellow("SlowDown") {
+        @Override
+        public void mean() {
+            System.out.println("속도를 낮추세요!");
+        }
+    };
+    private String trafficLight;
+
+    protected abstract void mean();
+
+    TrafficLight(String light) {
+        this.trafficLight = light;
+    }
+
+    public String GetTrafficLight() {
+        return trafficLight;
+    }
+}
