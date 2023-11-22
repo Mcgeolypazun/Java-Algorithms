e4421aa98f9b21f35988d2f7026d607648166e54
Mcgeolypazun <taratula0504@gmail.com>
2023-11-22 09:46:24 +0900

    feat : 내배캠 알고리즘 32~34번 풀이

---
+++ src/november21th/Solution2.java
@@ -0,0 +1,65 @@
+package november21th;
+
+import java.util.Scanner;
+
+public class Solution2 {
+
+    int countFactors(int n){
+        int answer = 0;
+        for(int i=1;i<=n;i++){
+            if(n % i == 0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+    boolean checkFactors(int n){
+        if(n % 2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public int solution(int left, int right) {
+        int answer = 0;
+
+       for(int i=left;i<=right;i++){
+           if(checkFactors(countFactors(i))){
+               answer += i;
+           }
+           else{
+               answer -= i;
+           }
+       }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution2 T = new Solution2();
+        Scanner kb = new Scanner(System.in);
+        int aa = kb.nextInt();
+        int bb = kb.nextInt();
+        System.out.println(T.solution(aa,bb));
+    }
+}
+
+//class Solution {
+//    public int solution(int left, int right) {
+//        int answer = 0;
+//
+//        for (int i=left;i<=right;i++) {
+//            //제곱수인 경우 약수의 개수가 홀수
+//            if (i % Math.sqrt(i) == 0) {
+//                answer -= i;
+//            }
+//            //제곱수가 아닌 경우 약수의 개수가 짝수
+//            else {
+//                answer += i;
+//            }
+//        }
+//
+//        return answer;
+//    }
+//}
---
+++ src/november21th/Solution3.java
@@ -0,0 +1,28 @@
+package november21th;
+
+import November4th.Main;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution3 {
+    public String solution(String s) {
+        String answer = "";
+        char charArr[] = s.toCharArray();
+
+       for(int i=0;i<charArr.length;i++){
+           for(int j=0;j<charArr.length;j++){
+
+           }
+       }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution3 T = new Solution3();
+        Scanner kb = new Scanner(System.in);
+        System.out.println(kb.next());
+    }
+}
